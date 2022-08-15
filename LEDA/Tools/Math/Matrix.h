52f8c1237711e39a0b71f500664ff37490adbe93
CSP-PC <csptpc@gmail.com>
2022-08-15 22:54:36 +0800

    matrix.h declarations

--- LEDA/Tools/Math/Matrix.h
+++ LEDA/Tools/Math/Matrix.h
@@ -12,5 +12,38 @@
 #pragma once
 
 namespace LEDA {
-    
+    typedef union {
+        // data members
+        struct {
+            double m00, m01, m02,
+                m10, m11, m12,
+                m20, m21, m22;
+        };
+        double m[9];
+        double m2[3][3];
+
+        // constructors
+        Mtx33();
+        Mtx33(const double* arr);
+        Mtx33(double _00, double _01, double _02, double _10, double _11, double _12, double _20, double _21, double _22);
+
+        // assignment operators
+        Mtx33& operator*= (Mtx33 const& rhs); // performs matrix multiplication
+
+        // member functions
+        Mtx33 transpose() const;
+        Mtx33 inverse() const;
+
+        // static functions
+        static Mtx33 identity();
+        static Mtx33 translate();
+        static Mtx33 scale();
+        static Mtx33 rotateDeg();
+        static Mtx33 rotateRad();
+
+    } Matrix3x3, Mtx3x3, Mtx33;
+
+    // binary operators
+    Mtx33 operator*(Mtx33 const& lhs, Mtx33 const& rhs); // matrix multiplication
+    Vec2D operator*(Mtx33 const& lhs, Vec2D const& rhs); // applying matrix to vector
 }
--- LEDA/Tools/Math/Vector.h
+++ LEDA/Tools/Math/Vector.h
@@ -37,6 +37,22 @@ namespace LEDA {
 		Vec2D normalize() const;
 		Vec2D outward_normal() const;
 		double length() const;
+		double squareLength() const;
+
+		// static functions
+		static distance(Vec2D vec0, Vec2D vec1); // get the distance between two points represented as vectors
+		static squareDistance(Vec2D vec0, Vec2D vec1);
+		static dotProduct(Vec2D vec0, Vec2D vec1);
+		static crossProductMagnitude(Vec2D vec0, Vec2D vec1);
 
 	} Vec2D, Vector2D, Vec2;;
+
+	// binary operators
+	Vec2D operator+(Vec2D const& lhs, Vec2D const& rhs); // vector addition
+	Vec2D operator-(Vec2D const& lhs, Vec2D const& rhs); // vector subtraction
+	Vec2D operator*(Vec2D const& lhs, double rhs);
+	Vec2D operator*(double lhs, Vec2D const& rhs); // scalar multiplication
+	Vec2D operator/(Vec2D const& lhs, double rhs); // scalar division
+
+	// 
 }
