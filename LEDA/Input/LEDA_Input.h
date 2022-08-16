c1aa239541a959b9582a240b9de396916cad4002
Haloen <72333894+Haloen@users.noreply.github.com>
2022-08-16 13:20:33 +0800

    I Forgor about input

---
+++ LEDA/Input/LEDA_Input.h
@@ -0,0 +1,23 @@
+/**
+ * @file        LEDA_Graphics.h
+ * @author      your name (you@domain.com)
+ * @brief       Defines Functions to render Game Objects
+ * @version     0.1
+ * @date        16-08-2022
+ * 
+ * @copyright   Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+
+namespace LEDA {
+
+    bool isPressed(Key);
+    bool isTriggered(Key);
+    bool isReleased(Key);
+
+    enum KEY {
+
+    }
+}
