d407d9cb7255d3c5a0dbde31e83c1ae286f6198d
h1810126 <h1810126@nushigh.edu.sg>
2023-01-31 21:43:19 +0800

    create pong project (still quite empty)

---
+++ Pong/Pong.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file        Pong.cpp
+ * @author      your name (you@domain.com)
+ * @brief       ssssssssssssssssssssssssss (nake)
+ * @version     0.1
+ * @date        01-01-2023
+ *
+ * @copyright   Copyright (c) 2023
+ */
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include "LEDA_Graphics.h"
+#include "LEDA_Input.h"
+
+using namespace LEDA;
+
+#include <vector>
