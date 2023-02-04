afe8780e68cb2ad86327416ddf8e9fbc32b86138
h1810126 <h1810126@nushigh.edu.sg>
2023-02-04 16:38:32 +0800

    move the paddle

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -21,7 +21,41 @@ using namespace LEDA;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
+double paddle_vx = 0.0;
+double paddle_size = 100.0;
+
+void background_update() {
+
+    GameObject* paddle = retrieveGameObject("paddle");
+    TransformComponent* p_tc = getComponent<TransformComponent>(paddle);
+    KinematicsComponent* p_kc = getComponent<KinematicsComponent>(paddle);
+
+    p_tc->scale.x = paddle_size;
+    p_kc->vel.x = paddle_vx;
+
+}
+
 void _init() {
+
+    addKeyTriggerCallback(INPUT_KEY::KEY_UP, []() {
+        // nothing (for now)
+    });
+    addKeyTriggerCallback(INPUT_KEY::KEY_RIGHT, []() {
+        paddle_vx += 200;
+    });
+    addKeyTriggerCallback(INPUT_KEY::KEY_LEFT, []() {
+        paddle_vx -= 200;
+    });
+    addKeyReleaseCallback(INPUT_KEY::KEY_RIGHT, []() {
+        paddle_vx -= 200;
+    });
+    addKeyReleaseCallback(INPUT_KEY::KEY_LEFT, []() {
+        paddle_vx += 200;
+    });
+
+    GameObject* background = retrieveGameObject("background");
+    LogicComponent* lc = getComponent<LogicComponent>(background);
+    lc->update = background_update;
 
 }
 
